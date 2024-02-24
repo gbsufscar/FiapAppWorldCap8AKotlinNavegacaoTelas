9500a304db25721caaabf9a9359fd7a752bb8284
Gustavo Bernardo da Silva <gbsufscar@hotmail.com>
2024-02-24 16:28:54 -0300

    Criação do projeto com inclusão da Dependência do Navigatio: implementatio androidx.navigation:navigation-compose:2.7.7 no build.gradle.kts (Module :app).

---
+++ app/src/main/java/br/com/fiap/navegandoentretelas/MainActivity.kt
@@ -0,0 +1,44 @@
+package br.com.fiap.navegandoentretelas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.fiap.navegandoentretelas.ui.theme.NavegandoEntreTelasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavegandoEntreTelasTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    /* TODO */
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    /* TODO */
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NavegandoEntreTelasTheme {
+        /* TODO */
+    }
+}
